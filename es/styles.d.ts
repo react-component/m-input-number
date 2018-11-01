d374a08c9c3e75ba4ba677ed4640b6715b2987bc
fxxkscript <ray@tomo.im>
2018-11-01 10:03:05 +0800

    build

---
+++ es/styles.d.ts
@@ -0,0 +1,13 @@
+import { ImageStyle, TextStyle, ViewStyle } from 'react-native';
+export { ImageStyle, TextStyle, ViewStyle };
+declare const _default: {
+    container: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    input: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    stepWrap: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    stepText: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    stepDisabled: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    disabledStepTextColor: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    highlightStepTextColor: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+    highlightStepBorderColor: import("react-native").RegisteredStyle<ViewStyle | TextStyle | ImageStyle>;
+};
+export default _default;
